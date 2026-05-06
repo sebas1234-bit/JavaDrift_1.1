0f80257f184b0240d037ba5964aedc3a14ead00e
Sebas_programer <sebastianhernandezlondono623@gmail.com>
2026-05-05 21:36:30 -0500

    carro del jugador moviendose con las flechas del teclado

--- src/com/javadrift/GamePanel.java
+++ src/com/javadrift/GamePanel.java
@@ -10,11 +10,18 @@ public class GamePanel extends JPanel implements Runnable {
     final int ANCHO = 800;
     final int ALTO = 600;
 
+    int carroX = 375;
+    int carroY = 275;
+    int velocidad = 3;
+
     Thread hiloJuego;
+    KeyHandler teclado = new KeyHandler();
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(ANCHO, ALTO));
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.DARK_GRAY);
         this.setFocusable(true);
+        this.addKeyListener(teclado);
     }
 
     public void iniciarJuego() {
@@ -24,17 +31,37 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Game loop corriendo...");
+        while (hiloJuego != null) {
+            actualizar();
+            repaint();
+            try {
+                Thread.sleep(16);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void actualizar() {
+        if (teclado.arribaPresionada) carroY -= velocidad;
+        if (teclado.abajoPresionada) carroY += velocidad;
+        if (teclado.izquierdaPresionada) carroX -= velocidad;
+        if (teclado.derechaPresionada) carroX += velocidad;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.WHITE);
-        g.drawString("JavaDrift - Juego cargando...", 300, 300);
-    }
 
+        g.setColor(Color.GREEN);
+        g.fillRect(0, 0, ANCHO, ALTO);
+
+        g.setColor(Color.RED);
+        g.fillRect(carroX, carroY, 50, 30);
+    }
 }
+
+
 
 
 
---
+++ src/com/javadrift/KeyHandler.java
@@ -0,0 +1,31 @@
+package com.javadrift;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public boolean arribaPresionada, abajoPresionada;
+    public boolean izquierdaPresionada, derechaPresionada;
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int codigo = e.getKeyCode();
+
+        if (codigo == KeyEvent.VK_UP) arribaPresionada = true;
+        if (codigo == KeyEvent.VK_DOWN) abajoPresionada = true;
+        if (codigo == KeyEvent.VK_LEFT) izquierdaPresionada = true;
+        if (codigo == KeyEvent.VK_RIGHT) derechaPresionada = true;
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int codigo = e.getKeyCode();
+
+        if (codigo == KeyEvent.VK_UP)    arribaPresionada = false;
+        if (codigo == KeyEvent.VK_DOWN)  abajoPresionada = false;
+        if (codigo == KeyEvent.VK_LEFT)  izquierdaPresionada = false;
+        if (codigo == KeyEvent.VK_RIGHT) derechaPresionada = false;
+    }
+
+}
